3a6d1e276bb63cf3ddfd433138ef6d92c237482b
IaK3lwin <53442885+IaK3lwin@users.noreply.github.com>
2025-11-28 13:46:22 +0000

    Feart: improviments code

--- jogo-online/1st-realease/public/renderstate.js
+++ jogo-online/1st-realease/public/renderstate.js
@@ -1,23 +1,22 @@
-export default function renderstate(contextScreen, state) {
-    console.log("teste")
+export default function renderstate(contextScreen, game) {
     //clear screen
     contextScreen.clearRect(0,0, 10, 10) // mais performatico que apenas redesenhar um react ta tela inteira
 
 
-    for (const playerId in state.players) {
-            let currentPlayer = state.players[playerId]
+    for (const playerId in game.state.players) {
+            let currentPlayer = game.state.players[playerId]
         contextScreen.fillStyle = "black"
         contextScreen.fillRect(currentPlayer.x, currentPlayer.y, 1, 1)
     }
 
-    for (const fruitId in state.fruits) {
-        let currentFruit = state.fruits[fruitId]
+    for (const fruitId in game.state.fruits) {
+        let currentFruit = game.state.fruits[fruitId]
         contextScreen.fillStyle = "green"
         contextScreen.fillRect(currentFruit.x, currentFruit.y, 1,1)
         
     }
 
     requestAnimationFrame(() => {
-        renderstate(contextScreen, state)
+        renderstate(contextScreen, game)
     }) // chama o método, fazendo com que atualize a tela a todo frame
 }
